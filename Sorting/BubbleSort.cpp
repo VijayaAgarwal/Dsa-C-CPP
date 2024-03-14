fd644d9e0ea36d7586700142bf93b0707f4ff498
VijayaAgarwal <vijayaagarwal11@gmail.com>
2024-03-14 12:08:01 +0530

    file

---
+++ Sorting/BubbleSort.cpp
@@ -0,0 +1,32 @@
+//insertion sort on array
+#include<bits/stdc++.h>
+using namespace std;
+#define max_size 100
+int main(){
+    int arr[max_size],i,j,temp,n;
+    cout<<endl<<"Enter number of elements : ";
+    cin>>n;
+    cout<<"Enter elements : "<<endl;
+    for(i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"Elements before sorting : ";
+    for(i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    for(i=0;i<n;i++){
+        for(j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                 int temp=arr[j];
+                 arr[j]=arr[j+1];
+                 arr[j+1]=temp;
+            }
+        }   
+    }
+   cout<<endl<<"Elements of array after sorting are :"<<endl;
+    for(i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+   }
+}
+
+
